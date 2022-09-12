487dabc9b90dcccefdd3e896723f944ed42c2624
laurenhatchett <laurenhatchett@gmail.com>
2022-09-12 10:55:04 -0400

    store cached element references

--- js/app.js
+++ js/app.js
@@ -8,7 +8,11 @@ let board, turn, winner
 
 /*------------------ Cached Element References --------------*/
 
+const squareEls = document.querySelectorAll('.square')
 
+const messageEl = document.querySelector('#message')
+
+const resetBtnEl = document.querySelector('#reset-button')
 
 /*--------------------- Event Listeners -----------------------*/
 
